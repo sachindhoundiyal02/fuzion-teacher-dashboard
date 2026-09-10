02539489b17716e3ae289c06081b8df4288cd684
sachindhoundiyal02 <84776765+sachindhoundiyal02@users.noreply.github.com>
2026-09-10 22:54:26 +0530

    addfeepayment function change

--- firebase-config.js
+++ firebase-config.js
@@ -668,18 +668,20 @@ export const firebaseService = {
      * Never move the student's due date backwards.
      */
 
-    if (
-      !currentNextDueDate ||
-      nextDueDate > currentNextDueDate
-    ) {
+   if (
+  !currentNextDueDate ||
+  nextDueDate > currentNextDueDate
+) {
+  await firebaseFirestoreModule.updateDoc(
+    studentRef,
+    {
+      nextDueDate: nextDueDate,
 
-      await firebaseFirestoreModule.updateDoc(
-        studentRef,
-        {
-          nextDueDate: nextDueDate
-        }
-      );
+      // Mark the latest paid billing cycle as paid
+      lastFeePaidMonth: latestCycle.from.substring(0, 7)
     }
+  );
+}
   }
 
   return newPayment;
